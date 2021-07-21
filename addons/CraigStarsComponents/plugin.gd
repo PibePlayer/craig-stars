94ed00a822ba144054a80646bc895de8909b4b54
Craig Post <sirgwain@gmail.com>
2021-07-20 19:53:02 -0700

    Added new CSButton for callback enabled buttons

--- addons/CraigStarsComponents/plugin.gd
+++ addons/CraigStarsComponents/plugin.gd
@@ -2,9 +2,12 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-    var texture = preload("res://addons/CraigStarsComponents/icon.svg")
-    add_custom_type("ProductionQueueItemsTable", "MarginContainer", preload("res://addons/CraigStarsComponents/src/ProductionQueueItemsTable.cs"), texture)
-    
+	var texture = preload("res://addons/CraigStarsComponents/icon.svg")
+	var button_texture = preload("res://addons/CraigStarsComponents/assets/icon_button.svg")
+	add_custom_type("ProductionQueueItemsTable", "MarginContainer", preload("res://addons/CraigStarsComponents/src/ProductionQueueItemsTable.cs"), texture)
+	add_custom_type("CSButton", "Button", preload("res://addons/CraigStarsComponents/src/CSButton.cs"), button_texture)
+	
 func _exit_tree():
-    remove_custom_type("ProductionQueueItemsTable")
+	remove_custom_type("ProductionQueueItemsTable")
+	remove_custom_type("CSButton")
 	
---
+++ addons/CraigStarsComponents/src/CSButton.cs
@@ -0,0 +1,35 @@
+using CraigStarsTable;
+using Godot;
+using System;
+
+namespace CraigStars
+{
+    /// <summary>
+    /// Custom button type to allow for an event based button
+    /// </summary>
+    public class CSButton : Button
+    {
+        Action<CSButton> onPressed;
+
+        public override void _Ready()
+        {
+            base._Ready();
+            Connect("pressed", this, nameof(OnPressedCallback));
+        }
+
+        void OnPressedCallback()
+        {
+            onPressed?.Invoke(this);
+        }
+
+        /// <summary>
+        /// Register a callback with the "pressed" signal for this button
+        /// </summary>
+        /// <param name="onPressed">The action to callback</param>
+        public void OnPressed(Action<CSButton> onPressed)
+        {
+            this.onPressed = onPressed;
+        }
+
+    }
+}
